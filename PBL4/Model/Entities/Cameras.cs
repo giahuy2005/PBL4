fcea09289d688cdd24717498f9d550802bf745c7
giahuy2005 <huylegia860@gmail.com>
2025-11-26 13:16:24 +0700

    Chỉnh lại UI

--- PBL4/Model/Entities/Cameras.cs
+++ PBL4/Model/Entities/Cameras.cs
@@ -11,15 +11,15 @@ namespace PBL4.Model.Entities
         [PrimaryKey("id_camera", false)]
         public string? IdCamera { get; set; }
 
-        // Khóa ngoại, nên ánh xạ đúng tên cột DB 'id_user'
         [Column("id_user")]
         public string? IdUser { get; set; }
-
+        [Column("namecamera")]
         public string? NameCamera { get; set; }
-
+        [Column("url")]
         public string? URL { get; set; }
+        [Column("nameuser")]
         public string? NameUser { get; set; }
-
+        [Column("password")]
         public string? Password { get; set; }
 
         public User? User { get; set; }
